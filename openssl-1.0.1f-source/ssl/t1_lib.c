684bbfefbe4c43cc3c1cdc9f3a867150116c90f9
Qais Patankar <qaisjp@gmail.com>
2019-11-22 05:25:10 +0000

    patch heartbleed

--- openssl-1.0.1f-source/ssl/t1_lib.c
+++ openssl-1.0.1f-source/ssl/t1_lib.c
@@ -2559,9 +2559,13 @@ tls1_process_heartbeat(SSL *s)
 	unsigned int padding = 16; /* Use minimum padding */
 
 	/* Read type and payload length first */
+	if (1 + 2 + 16 > s->s3->rrec.length)
+		return 0;
 	hbtype = *p++;
 	n2s(p, payload);
 	pl = p;
+	if (1 + 2 + payload + 16 > s->s3->rrec.length)
+		return 0;
 
 	if (s->msg_callback)
 		s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT,
